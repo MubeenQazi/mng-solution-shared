c83cdab7785cfb8342b4558d150888cc2cc0788e
farjadakbar4 <farjadakbar4@gmail.com>
2022-12-23 11:58:02 +0500

    update

--- components/admin/Header/Header.tsx
+++ components/admin/Header/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
   React.useEffect(() => {
     // return () => {
     if (sessionStorage.getItem("expires_on") === null) {
-      navigate("/e=timeout");
+      navigate("/?e=timeout");
     }
     // };
   }, []);
