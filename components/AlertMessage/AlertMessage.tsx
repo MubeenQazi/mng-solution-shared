0ffcc987a8519104339ed19e31d3238efca61463
farjadakbar4 <farjadakbar4@gmail.com>
2022-12-26 11:58:40 +0500

    update

--- components/AlertMessage/AlertMessage.tsx
+++ components/AlertMessage/AlertMessage.tsx
@@ -13,11 +13,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function AlertMessage(
-  open: boolean = false,
-  message: string,
-  severity: string = "success"
-) {
+export default function AlertMessage(open: boolean = false, message: string) {
   const handleClose = (
     event?: React.SyntheticEvent | Event,
     reason?: string
@@ -28,11 +24,12 @@ export default function AlertMessage(
 
     open = false;
   };
+  let color = "success";
 
   return (
     <Stack spacing={2} sx={{ width: "100%" }}>
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+        <Alert onClose={handleClose} severity={color} sx={{ width: "100%" }}>
           {message}
         </Alert>
       </Snackbar>
