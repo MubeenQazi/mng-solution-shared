30b47f0f82a61e4dcaa4bc75e2332868bee2ed12
farjadakbar4 <farjadakbar4@gmail.com>
2022-12-28 12:30:27 +0500

    update

--- components/AlertMessage/AlertMessage.tsx
+++ components/AlertMessage/AlertMessage.tsx
@@ -17,9 +17,17 @@ export default function AlertMessage(
   message: string,
   alertcolor: any
 ) {
+  const [alert, setAlert] = React.useState(open);
+  React.useEffect(() => {
+    // when the component is mounted, the alert is displayed for 5 seconds
+    setTimeout(() => {
+      setAlert(false);
+    }, 5000);
+  }, [open]);
+
   return (
     <Stack spacing={2} sx={{ width: "100%" }}>
-      <Snackbar open={open} autoHideDuration={6000}>
+      <Snackbar open={alert} autoHideDuration={6000}>
         <Alert severity={alertcolor} sx={{ width: "100%" }}>
           {message}
         </Alert>
