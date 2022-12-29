567c39ef0bca54a9badece93ef9652241c2bf1b4
farjadakbar4 <farjadakbar4@gmail.com>
2022-12-29 12:09:21 +0500

    update

--- components/DownloadButton/DownloadButton.tsx
+++ components/DownloadButton/DownloadButton.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@mui/material/Button";
 import { CsvBuilder } from "filefy";
+import "../MsButton/MsButton.scss";
 
 const DownloadButton = ({ rows, columns, filename }: any) => {
   const downloadCSV = () => {
@@ -17,24 +18,7 @@ const DownloadButton = ({ rows, columns, filename }: any) => {
   };
 
   return (
-    <Button
-      size="small"
-      sx={{
-        paddingLeft: `30px`,
-        paddingRight: `30px`,
-        height: `50px`,
-        borderRadius: `50px`,
-        textTransform: `inherit`,
-        backgroundColor: "#FEB127",
-        font: `normal normal 500 20px/80px Raleway`,
-        "&:hover, &:active": {
-          backgroundColor: "#FEB127",
-          color: "#fff",
-        },
-      }}
-      variant="contained"
-      onClick={downloadCSV}
-    >
+    <Button size="small" variant="contained" onClick={downloadCSV}>
       Download
     </Button>
   );
